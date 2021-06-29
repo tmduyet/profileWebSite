dd6848c3affb0ba91772ce02a4a3ee8ea165e18b
tmduyet <tmduyet@csc.hcmus.edu.vn>
2021-06-29 22:59:46 +0700

    nav_bar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,20 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+     navClose=document.getElementById('nav-close')
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+    if(navToggle){
+        navToggle.addEventListener('click',()=>{
+            navMenu.classList.add('show-menu')
+        })
+    }
+    if(navClose){
+        navClose.addEventListener('click',()=>{
+            navMenu.classList.remove('show-menu')
+        })
+    }
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
